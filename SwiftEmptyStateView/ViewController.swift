bda70dd5bbc3f37eb868dec0634c469ccd632256
LeaderBoy <748603349@qq.com>
2019-11-15 10:08:56 +0800

    Initial Commit

---
+++ SwiftEmptyStateView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftEmptyStateView
+//
+//  Created by 杨志远 on 2019/11/15.
+//  Copyright © 2019 BaQiWL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
